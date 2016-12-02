618742bfe99d955b2b2262fa50d0ccf7cbe631b7
Григорий Ким <mail@kim-g.ru>
2016-12-03 00:50:44 +0500

    Закончил класс с информацией.
    Принял решение делать через заполнение шаблона.

--- Parser/Parser/CONTRACT.cs
+++ Parser/Parser/CONTRACT.cs
@@ -1,4 +1,7 @@
-﻿namespace Parser
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parser
 {
     public class CONTRACT : Serializer
     {
@@ -8,13 +11,23 @@
         public string DealerContractCode = "A";
         public string DealerContractDate = "A";
         public string ABSContractCode = "A";
-        public Customer CUSTOMER;
-        private string BANKPROPLIST = "";
+        public Customer CUSTOMER = new Customer();
+        public string BANKPROPLIST = "A";
+        public Delivery DELIVERY = new Delivery();
+        public Contact CONTACT = new Contact();
+        public Connections CONNECTIONS = new Connections();
+        public List<LOGPARAM> LOGPARAMS = new List<LOGPARAM>();
+        public string Comments = "A";
+        public string CLIENTVER = "A";
 
         public CONTRACT()
         {
-            CUSTOMER = new Customer();
-        }
+            LOGPARAMS.Add(new LOGPARAM());
+            LOGPARAMS.Add(new LOGPARAM());
+            LOGPARAMS.Add(new LOGPARAM());
+            LOGPARAMS.Add(new LOGPARAM());
+            LOGPARAMS.Add(new LOGPARAM());
+        } 
     }
 
     public class Customer
@@ -23,28 +36,16 @@
         public string SPHERESId = "A";
         public string Resident = "A";
         public string Ratepayer = "A";
-        public Person PERSON;
-        public Address ADDRESS;
-
-        public Customer()
-        {
-            PERSON = new Person();
-            ADDRESS = new Address();
-        }
+        public Person PERSON = new Person();
+        public Address ADDRESS = new Address();
     }
 
     public class Person
     {
         public string PERSONTYPESId = "A";
-        public PersonName PERSONNAME;
-        public Document DOCUMENT;
+        public PersonName PERSONNAME = new PersonName();
+        public Document DOCUMENT = new Document();
         public string INN = "";
-
-        public Person()
-        {
-            PERSONNAME = new PersonName();
-            DOCUMENT = new Document();
-        }
     }
 
     public class PersonName
@@ -81,4 +82,73 @@
         public string ROOMTYPESId = "";
         public string Room = "A";
     }
+
+    public class Delivery
+    {
+        public string DELIVERYTYPESId = "A";
+        public Address ADDRESS = new Address();
+        public string Notes = "A";
+    }
+
+    public class Contact
+    {
+        public PersonName PERSONNAME = new PersonName();
+        public string PhonePrefix = "A";
+        public string Phone = "A";
+        public string FaxPrefix = "A";
+        public string Fax = "A";
+        public string EMail = "A";
+        public string PagerOperatorPrefix = "A";
+        public string PagerOperator = "A";
+        public string PagerAbonent = "A";
+        public string Notes = "A";
+    }
+
+    public class Connections
+    {
+        public Connection CONNECTION = new Connection();
+    }
+
+    public class Connection
+    {
+        public string PAYSYSTEMSId = "A";
+        public string BILLCYCLESId = "A";
+        public string CELLNETSId = "A";
+        public string PRODUCTSId = "A";
+        public string PhoneOwner = "A";
+        public string SerNumber = "A";
+        public string SimLock = "A";
+        public string IMSI = "";
+        public Mobiles MOBILES = new Mobiles();
+    }
+
+    public class Mobiles
+    {
+        public Mobile MOBILE = new Mobile();
+    }
+
+    public class Mobile
+    {
+        public string CHANNELTYPESId = "A";
+        public string CHANNELLENSId = "A";
+        public string SNB = "A";
+        public string BILLPLANSId = "A";
+        public List<string> SERVICES = new List<string>();
+
+        public Mobile()
+        {
+            SERVICES.Add("1");
+            SERVICES.Add("6");
+            SERVICES.Add("8");
+            SERVICES.Add("34");
+        }
+    }
+
+    public class LOGPARAM
+    {
+        public string LOGPARAMSId = "A";
+        public string LOGPARAMSCode = "A";
+        public string LOGPARAMSValue = "A";
+    }
+
 }
