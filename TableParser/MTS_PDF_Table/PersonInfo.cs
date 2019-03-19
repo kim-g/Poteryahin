f75c86aa20cd0ed0bcfbc5fedd9cd686e91b7316
Григорий Ким <kim-g@yandex.ru>
2019-03-19 15:10:41 +0500

    Убрал ненужные директивы using

--- TableParser/MTS_PDF_Table/PersonInfo.cs
+++ TableParser/MTS_PDF_Table/PersonInfo.cs
@@ -1,12 +1,7 @@
 ﻿using iTextSharp.text.pdf;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 
 namespace MTS_PDF_Table
 {
