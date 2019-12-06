334127a8ec151fb9791a5865cfe9fc89807a2664
Григорий Ким <kim-g@yandex.ru>
2019-12-06 14:19:35 +0500

    Внесены изменения в шаблон бланка МТС
    Добавлена функция правки шрифта при заполнении формы для отображения кириллицы
    
    Добавлена функция генерации штрих-кода из внутреннего JavaScript формы.

--- TableParser/MTS_PDF_Table/PersonInfo.cs
+++ TableParser/MTS_PDF_Table/PersonInfo.cs
@@ -1,7 +1,9 @@
-﻿using iTextSharp.text.pdf;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
 using System;
 using System.Data;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace MTS_PDF_Table
@@ -23,6 +25,7 @@ namespace MTS_PDF_Table
         public string[] DocumentIssuedBy { get; set; } = new string[2];
         public DateTime DocumentIssueDate { get; set; }
         public string DocumentIndex { get; set; }
+        public string[] DocumentIndex2 { get; set; }
         public DateTime Birth { get; set; }
         public string PlaceIndex { get; set; }
         public string PlaceCity { get; set; }
@@ -62,7 +65,7 @@ namespace MTS_PDF_Table
                             Number = InTable.Rows[Position].ItemArray[Col.Ordinal].ToString();
                             Number = Number.Replace("-", "").Replace("(", "").Replace(")", "");
                             Regex phone = new Regex(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?([\d\- ]{3})([\d\- ]{4,5})$");
-                            Number = phone.Replace(Number, "($3) $4-$5");
+                            Number = phone.Replace(Number, "$3$4$5");
                         }
                         catch (Exception e)
                         {
@@ -229,10 +232,12 @@ namespace MTS_PDF_Table
                         try
                         {
                             DocumentIndex = InTable.Rows[Position].ItemArray[Col.Ordinal].ToString();
+                            DocumentIndex2 = DocumentIndex.Split('-');
                         }
                         catch (Exception e)
                         {
                             DocumentIndex = "";
+                            DocumentIndex2 = new string[] { "", "" };
                             ToLog(InTable.TableName, Position, Col.Caption, e.Message);
                         }
                         break;
@@ -367,26 +372,124 @@ namespace MTS_PDF_Table
                                 $"столбец {ColumnName}: Ошибка чтения данных: {ErrorText}");
         }
 
+        private string OnlyNum(string Text)
+        {
+            Regex ON = new Regex(@"/[^\d]+/g");
+            return ON.Replace(Text, "");
+        }
+
+        private string charcode(int code)
+        {
+            string Res = CharCode(code);
+            return Res.Substring(0, 1);
+        }
+
+        private string CharCode(int code)
+        {
+            ToLog("CharCode", 0, "", code.ToString());
+
+            switch (code)
+            {
+                case 0: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8364)));
+                case 95: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8216)));
+                case 96: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8217)));
+                case 97: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8220)));
+                case 98: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8221)));
+                case 99: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8226)));
+                case 100: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8211)));
+                case 101: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(8212)));
+                case 102: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(732)));
+                default: return new string(Encoding.Unicode.GetChars(BitConverter.GetBytes(code + 32)));
+            }
+        }
+
         public void FillForm(string newFile)
         {
+            var pdfFile = newFile;
+
+            using (var templateReader = new PdfReader(pdfTemplate))
+            {
+                using (var resultStamper = new PdfStamper(templateReader,
+                    new FileStream(pdfFile, FileMode.Create)))
+                {
+                    // Получаем ссылку на форму с полями.
+                    var form = resultStamper.AcroFields;
+                    // Получаем все шрифты формы.
+                    var fonts = templateReader.GetFormFonts();
+
+                    // Установка значений полей формы.
+                    form.SetFieldWithFont(templateReader, fonts, "02", Rate);
+                    form.SetFieldWithFont(templateReader, fonts, "03", Surname);
+                    form.SetFieldWithFont(templateReader, fonts, "04", Name);
+                    form.SetFieldWithFont(templateReader, fonts, "05", SecondName);
+                    form.SetFieldWithFont(templateReader, fonts, "06", Birth.Day.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "07", Birth.Month.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "08", Birth.Year.ToString("D4"));
+                    form.SetField("09", Sex ? "1" : "0");
+                    form.SetField("10", Sex ? "0" : "1");
+                    form.SetFieldWithFont(templateReader, fonts, "11", BirthPlace);
+                    form.SetFieldWithFont(templateReader, fonts, "13", Citizenship);
+                    form.SetFieldWithFont(templateReader, fonts, "12", Document);
+                    form.SetFieldWithFont(templateReader, fonts, "14", DocumentSerie);
+                    form.SetFieldWithFont(templateReader, fonts, "15", DocumentNumber);
+                    form.SetFieldWithFont(templateReader, fonts, "16", DocumentIssuedBy[0] + DocumentIssuedBy[1]);
+                    //form.SetFieldWithFont(templateReader, fonts, "17", DocumentIssuedBy[1]);
+                    form.SetFieldWithFont(templateReader, fonts, "17", DocumentIssueDate.Day.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "18", DocumentIssueDate.Month.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "19", DocumentIssueDate.Year.ToString("D4"));
+                    form.SetFieldWithFont(templateReader, fonts, "20", DocumentIndex2[0]);
+                    form.SetFieldWithFont(templateReader, fonts, "21", PlaceIndex);
+                    form.SetFieldWithFont(templateReader, fonts, "22", DocumentIndex2[1]);
+                    form.SetFieldWithFont(templateReader, fonts, "25", PlaceCity);
+                    form.SetFieldWithFont(templateReader, fonts, "26", PlaceStreet + ", " + PlaceBuilding + ", " + PlaceFlat);
+                    form.SetFieldWithFont(templateReader, fonts, "49", SellerID);
+                    form.SetFieldWithFont(templateReader, fonts, "51", Seller);
+                    form.SetFieldWithFont(templateReader, fonts, "iccid", ICC + "-" + ICC_Suffix);
+                    form.GetRevision("iccid");
+                    form.SetFieldWithFont(templateReader, fonts, "phone", Number);
+                    form.GetRevision("phone");
+                    form.SetFieldWithFont(templateReader, fonts, "60", ContractConclusionDate.Day.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "61", ContractConclusionDate.Month.ToString("D2"));
+                    form.SetFieldWithFont(templateReader, fonts, "62", ContractConclusionDate.Year.ToString("D4"));
+                    form.SetFieldWithFont(templateReader, fonts, "63", ContractConclusionPlace);
+
+                    string Text = OnlyNum(ICC + ICC_Suffix + Number);
+                    int check = 105;
+                    string result = "";
+                    for (var i = 0; i < Text.Length / 2; i++)
+                    {
+                        var substr = Text.Substring(i * 2, 2);
+                        int intval = Convert.ToInt32(substr);
+                        check = check + intval * (i + 1);
+                        result = result + charcode(intval);
+                    }
+                    check = check % 103;
+                    result = "›" + result + charcode(check) + "œ";
+
+                    ToLog("CharCode", 0, "", result);
+
+                    form.SetField("barcode", result);
+
+                    // Установка запрета на редактирование полей.
+                    //resultStamper.FormFlattening = true;
+
+                    resultStamper.Close();
+                }
+                templateReader.Close();
+            }
+            
+            /*
+            FontFactory.RegisterDirectory(Environment.GetFolderPath(Environment.SpecialFolder.Fonts));
+
             PdfReader pdfReader = new PdfReader(pdfTemplate);
             PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileStream(
                 newFile, FileMode.Create));
             AcroFields pdfFormFields = pdfStamper.AcroFields;
-            // set form pdfFormFields
-            // The first worksheet and W-4 form
 
-            /*String[] values = pdfFormFields.GetAppearanceStates("10");
-            StringBuilder sb = new StringBuilder();
-            foreach (string value in values)
-            {
-                sb.Append(value);
-                sb.Append('\n');
-            }
-            MessageBox.Show(sb.ToString());*/
+            pdfFormFields.AddSubstitutionFont(BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.WINANSI, false));
 
-            pdfFormFields.SetField("02", Rate);
-            pdfFormFields.SetField("03", Surname);
+            //pdfFormFields.SetField("02",  Rate, Rate, true);
+            //pdfFormFields.SetField("03", Surname);
             pdfFormFields.SetField("04", Name);
             pdfFormFields.SetField("05", SecondName);
             pdfFormFields.SetField("06", Birth.Day.ToString("D2"));
@@ -395,28 +498,28 @@ namespace MTS_PDF_Table
             pdfFormFields.SetField("09", Sex ? "1" : "0");
             pdfFormFields.SetField("10", Sex ? "0" : "1");
             pdfFormFields.SetField("11", BirthPlace);
-            pdfFormFields.SetField("12", Citizenship);
-            pdfFormFields.SetField("13", Document);
+            pdfFormFields.SetField("13", Citizenship);
+            pdfFormFields.SetField("12", Document);
             pdfFormFields.SetField("14", DocumentSerie);
             pdfFormFields.SetField("15", DocumentNumber);
-            pdfFormFields.SetField("16", DocumentIssuedBy[0]);
-            pdfFormFields.SetField("17", DocumentIssuedBy[1]);
-            pdfFormFields.SetField("18", DocumentIssueDate.Day.ToString("D2"));
-            pdfFormFields.SetField("19", DocumentIssueDate.Month.ToString("D2"));
-            pdfFormFields.SetField("20", DocumentIssueDate.Year.ToString("D4"));
-            pdfFormFields.SetField("21", DocumentIndex);
-            pdfFormFields.SetField("22", PlaceIndex);
-            pdfFormFields.SetField("24", PlaceCity);
-            pdfFormFields.SetField("25", PlaceStreet + ", " + PlaceBuilding + ", " + PlaceFlat);
-            pdfFormFields.SetField("58", SellerID);
-            pdfFormFields.SetField("59", Seller);
-            pdfFormFields.SetField("60", ICC);
-            pdfFormFields.SetField("61", ICC_Suffix);
-            pdfFormFields.SetField("62", Number);
-            pdfFormFields.SetField("66", ContractConclusionDate.Day.ToString("D2"));
-            pdfFormFields.SetField("67", ContractConclusionDate.Month.ToString("D2"));
-            pdfFormFields.SetField("68", ContractConclusionDate.Year.ToString("D4"));
-            pdfFormFields.SetField("69", ContractConclusionPlace);
+            pdfFormFields.SetField("16", DocumentIssuedBy[0] + DocumentIssuedBy[1]);
+            //pdfFormFields.SetField("17", DocumentIssuedBy[1]);
+            pdfFormFields.SetField("17", DocumentIssueDate.Day.ToString("D2"));
+            pdfFormFields.SetField("18", DocumentIssueDate.Month.ToString("D2"));
+            pdfFormFields.SetField("19", DocumentIssueDate.Year.ToString("D4"));
+            pdfFormFields.SetField("20", DocumentIndex2[0]);
+            pdfFormFields.SetField("21", PlaceIndex);
+            pdfFormFields.SetField("22", DocumentIndex2[1]);
+            pdfFormFields.SetField("25", PlaceCity);
+            pdfFormFields.SetField("26", PlaceStreet + ", " + PlaceBuilding + ", " + PlaceFlat);
+            pdfFormFields.SetField("49", SellerID);
+            pdfFormFields.SetField("51", Seller);
+            pdfFormFields.SetField("iccid", ICC+"-"+ ICC_Suffix);
+            pdfFormFields.SetField("phone", Number);
+            pdfFormFields.SetField("60", ContractConclusionDate.Day.ToString("D2"));
+            pdfFormFields.SetField("61", ContractConclusionDate.Month.ToString("D2"));
+            pdfFormFields.SetField("62", ContractConclusionDate.Year.ToString("D4"));
+            pdfFormFields.SetField("63", ContractConclusionPlace);
 
             // flatten the form to remove editting options, set it to false
             // to leave the form open to subsequent manual edits
@@ -424,7 +527,7 @@ namespace MTS_PDF_Table
             pdfStamper.FormFlattening = false;
             // close the pdf
 
-            pdfStamper.Close();
+            pdfStamper.Close();*/
         }
     }
 }
